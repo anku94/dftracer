2dc6cbd2c0ad3d6437aa428d1a260d70fe80bb2a
hariharandev1 <hariharandev1@llnl.gov>
2025-03-24 11:07:50 -0700

    Add CI scripts and configuration for DLIO benchmark testing

---
+++ .gitlab/scripts/pre.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Enable debugging to echo commands being run
+set -x
+
+# Print the hostname where the script is being executed
+echo "Running pre.sh on $(hostname) by $USER on $PWD"
+
+# Load the required modules for Python, MPI, and GCC
+echo "Loading modules: Python ($PYTHON_MODULE), MPI ($MPI_MODULE), and GCC ($GCC_MODULE)"
+module load $PYTHON_MODULE $MPI_MODULE $GCC_MODULE
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to load modules."
+    exit 1
+fi
+
+# Check the values of the loaded modules
+echo "PYTHON_MODULE: $PYTHON_MODULE"
+echo "MPI_MODULE: $MPI_MODULE"
+echo "GCC_MODULE: $GCC_MODULE"
+
+# Check the values of the environment variables
+echo "CUSTOM_CI_ENV_DIR: $CUSTOM_CI_ENV_DIR"
+echo "ENV_NAME: $ENV_NAME"
+
+# Create a new Python virtual environment (if not exist) in the specified directory
+if [[ ! -d $CUSTOM_CI_ENV_DIR/$ENV_NAME ]]; then
+    echo "Creating a new Python virtual environment at $CUSTOM_CI_ENV_DIR/$ENV_NAME"
+    python -m venv $CUSTOM_CI_ENV_DIR/$ENV_NAME
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create Python virtual environment."
+        exit 1
+    fi
+fi
+
+# Activating environment
+echo "Activating env at $CUSTOM_CI_ENV_DIR/$ENV_NAME"
+. $CUSTOM_CI_ENV_DIR/$ENV_NAME/bin/activate
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to activate Python virtual environment."
+    exit 1
+fi
+
+# Upgrade pip
+pip install --upgrade pip
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to upgrade pip."
+    exit 1
+fi
+
+# Disable debugging
+set +x
---
+++ .gitlab/scripts/variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CUSTOM_CI_ENV_DIR=$(eval "echo $CUSTOM_CI_ENV_DIR")
+export CUSTOM_CI_OUTPUR_DIR=$(eval "echo $CUSTOM_CI_OUTPUR_DIR")
+export DATA_PATH=$(eval "echo $DATA_PATH")
