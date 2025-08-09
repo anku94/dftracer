4b1efa7a2f5d898aa3f5d698f8a4ad47ff89832a
hariharandev1 <hariharandev1@llnl.gov>
2025-08-09 11:29:07 -0700

    Add file locking in STDIOWriter and enhance child process handling in tests

--- src/dftracer/writer/stdio_writer.h
+++ src/dftracer/writer/stdio_writer.h
@@ -44,7 +44,13 @@ class STDIOWriter {
   // Write data to buffer, flush if necessary
   size_t write(const char* data, size_t len, bool force = false) {
     if (force || len >= max_size_) {
+      // Use stdio file locking (flockfile/funlockfile) for FILE*
+      // needed for fork and spawn cases to maintain consistency
+      // Note this may not work with nfs and should typically either create a
+      // new file per fork or use a local filesystem which supports flockfile.
+      flockfile(fh_);
       auto written = std::fwrite(data, 1, len, fh_);
+      funlockfile(fh_);
       if (written != len) {
         DFTRACER_LOG_ERROR("unable to write log file %s",
                            this->filename);  // GCOVR_EXCL_LINE
